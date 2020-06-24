8d459df99535e7caab4ff0ed9aed6d9d1895977a
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-06-24 18:12:41 -0300

    Changed parameters to nullable

--- braspag3ds/src/main/java/br/com/braspag/internal/data/EnrollResult.kt
+++ braspag3ds/src/main/java/br/com/braspag/internal/data/EnrollResult.kt
@@ -4,13 +4,13 @@ import br.com.braspag.internal.network.dto.Authentication
 
 internal data class EnrollResult (
     val status: EnrollStatus,
-    val authentication: Authentication?,
+    val authentication: Authentication? = null,
     val message: String,
-    val transactionId: String?,
-    val paReq: String?,
-    val xid: String?,
-    val eci: String?,
-    val cavv: String?,
-    val version: String?,
-    val referenceId: String?
+    val transactionId: String? = null,
+    val paReq: String? = null,
+    val xid: String? = null,
+    val eci: String? = null,
+    val cavv: String? = null,
+    val version: String? = null,
+    val referenceId: String? = null
 )
