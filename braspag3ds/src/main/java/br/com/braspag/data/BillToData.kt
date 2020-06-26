cdcff6a61e6d1be9c41798bcabf55a22bbf145ee
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-06-26 13:52:45 -0300

    Changed street2 to nullable

--- braspag3ds/src/main/java/br/com/braspag/data/BillToData.kt
+++ braspag3ds/src/main/java/br/com/braspag/data/BillToData.kt
@@ -2,13 +2,13 @@ package br.com.braspag.data
 
 data class BillToData(
     val customerId: String? = null,
-    val contactName: String,
+    val contactName: String? = null,
     val phoneNumber: String,
     val email: String,
     val street1: String,
-    val street2: String,
+    val street2: String? = null,
     val city: String,
     val state: String,
     val zipCode: String,
-    val country: String = "BR"
+    val country: String
 )
