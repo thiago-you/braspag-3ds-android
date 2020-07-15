ecb109ed1e81ebd7a665db494dfd32c73f75cfb7
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-07-15 13:00:50 -0300

    Updated list parameters to null instead of empty list

--- braspag3ds/src/main/java/br/com/braspag/data/AirlineData.kt
+++ braspag3ds/src/main/java/br/com/braspag/data/AirlineData.kt
@@ -1,8 +1,8 @@
 package br.com.braspag.data
 
 data class AirlineData(
-    val travelLeg: List<TravelLeg>? = listOf(),
-    val passenger: List<Passenger>? = listOf(),
+    val travelLeg: List<TravelLeg>? = null,
+    val passenger: List<Passenger>? = null,
     val numberOfPassengers: Long? = null,
     val billToPassportCountry: String? = null,
     val billToPassportNumber: String? = null
